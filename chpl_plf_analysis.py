15c934641a4c7a54af332db7c5fce9b09ccb47f0
H Richards <richardshgk@gmail.com>
2023-08-03 18:17:17 -0400

    first line graph

--- chpl_plf_analysis.py
+++ chpl_plf_analysis.py
@@ -5,7 +5,23 @@ import seaborn as sns
 
 # Read in data files
 chpl = pd.read_excel(r'assets/chpl_annual_2012-22_totals.xlsx')
-plf = pd.read_csv(r'assets/plf_annuals.csv')
+plf_annuals = pd.read_csv(r'assets/plf_annuals.csv')
 
-# print(chpl)
-# print(plf)
+# Assign variables for values
+circ = chpl["Circ YTD"]
+visits = chpl["Visits YTD"]
+computers = chpl["PC Usage YTD"]
+wifi = chpl["WiFi Usage YTD"]
+plf = plf_annuals["Amount"]
+year = plf_annuals["Year"]
+
+# Seaborn visuals
+sns.set()
+
+plt.plot(year, plf)
+plt.plot(year, visits)
+
+plt.xlabel('Calendar Year')
+plt.ylabel('Amount in USD')
+
+plt.show()
