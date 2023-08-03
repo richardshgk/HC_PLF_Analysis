38b02f416df2c5a19eb8421f6dc51d9c3b40a40d
H Richards <richardshgk@gmail.com>
2023-08-03 16:30:19 -0400

    updating packages

--- chpl_plf_analysis.py
+++ chpl_plf_analysis.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Read in data files
 chpl = pd.read_excel(r'assets/chpl_annual_2012-22_totals.xlsx')
 plf = pd.read_csv(r'assets/plf_annuals.csv')
 
 # print(chpl)
+# print(plf)
