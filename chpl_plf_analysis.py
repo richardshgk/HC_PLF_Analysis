0fe82ec82b1b60b6ed22e57bec78658ff950e2fe
H Richards <richardshgk@gmail.com>
2023-08-04 01:06:24 -0400

    created stacked hor bar chart

--- chpl_plf_analysis.py
+++ chpl_plf_analysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 import seaborn as sns
 
 # Read in data files
@@ -12,16 +13,32 @@ circ = chpl["Circ YTD"]
 visits = chpl["Visits YTD"]
 computers = chpl["PC Usage YTD"]
 wifi = chpl["WiFi Usage YTD"]
+
 plf = plf_annuals["Amount"]
 year = plf_annuals["Year"]
 
 # Seaborn visuals
 sns.set()
+plt.style.use("ggplot")
 
-plt.plot(year, plf)
-plt.plot(year, visits)
+# plt.bar(year, plf)
 
-plt.xlabel('Calendar Year')
-plt.ylabel('Amount in USD')
+# plt.title('Ohio Public Library Funds by Year')
+# plt.xlabel('Calendar Year')
+# plt.ylabel('PLF Money Awarded in USD')
+
+plt.barh(year, visits)
+plt.barh(year, circ, left=visits)
+plt.barh(year, computers, left=visits+circ)
+plt.barh(year, wifi, left=visits+circ+computers)
+
+plt.legend(['Visits', 'Circulation', 'PC Usage', 'Wifi Usage'])
+plt.yticks(ticks=year)
+
+
+plt.title('CHPL Usage by Year')
+plt.ylabel('Calendar Year')
+plt.xlabel('Instances of Sevices Used')
+# plt.yticks(np.arange(0, 30000000, step=))
 
 plt.show()
